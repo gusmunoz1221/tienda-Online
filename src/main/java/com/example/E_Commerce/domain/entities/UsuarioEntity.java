51d5133e5c8df8e7999581eeeb688200c3ed9ff4
gustavo <gustavito12217@gmail.com>
2024-09-26 19:24:39 -0300

    agregando los atributos faltabtes en EntityUsuario

--- src/main/java/com/example/E_Commerce/domain/entities/UsuarioEntity.java
+++ src/main/java/com/example/E_Commerce/domain/entities/UsuarioEntity.java
@@ -13,7 +13,10 @@ import org.antlr.v4.runtime.misc.NotNull;
 
 @MappedSuperclass  //no crea una tabla para Usuario, permitiendo que las clases hijas hereden sus atributos --> ESPECIALIZACION
 public abstract class UsuarioEntity {
-
     private String nombre;
+    private String correoElectronico;
     private String contraseña;
+
+    private String direccion;
+
 }
--- src/main/java/com/example/E_Commerce/infraestructura/services/UsuarioService.java
+++ src/main/java/com/example/E_Commerce/infraestructura/services/UsuarioService.java
@@ -1,4 +1,15 @@
 package com.example.E_Commerce.infraestructura.services;
 
+import com.example.E_Commerce.domain.repositories.CarritoRepository;
+import com.example.E_Commerce.domain.repositories.ClienteRepository;
+
 public class UsuarioService {
+    private final ClienteRepository clienteRepository;
+    public final CarritoRepository carritoRepository;
+    public UsuarioService(ClienteRepository clienteRepository, CarritoRepository carritoRepository) {
+        this.clienteRepository = clienteRepository;
+        this.carritoRepository = carritoRepository;
+    }
+
+
 }
